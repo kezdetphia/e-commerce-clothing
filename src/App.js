b57defc5ff038d25304d59fbaea8546038cca1ad
Fehér Márk <fehermark@marks-macbook.mynetworksettings.com>
2022-12-08 18:28:03 -0500

    set up core components

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import {Routes, Route} from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = ()=>{
+  return(
+    <div>
+      im the shop
+    </div>
+  )
+}
+
+
+const App = () => {
+  return(
+
+    <Routes>
+    
+      <Route path="/" element={<Navigation />} >
+        <Route index element={<Home/> } />
+        <Route path="shop" element={<Shop/> } />
+      </Route>
+
+    </Routes>
+
+
+
+
+  
+
+  )
+}
+
+
+export default App;
